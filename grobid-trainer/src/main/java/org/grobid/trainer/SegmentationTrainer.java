6937df917ff2a32cd7493ce5f1e7054f69d3b3cd
Luca Foppiano <luca@foppiano.org>
2026-03-21 11:34:55 +0100

    fix: revise segmentation trainer

--- grobid-trainer/src/main/java/org/grobid/trainer/SegmentationTrainer.java
+++ grobid-trainer/src/main/java/org/grobid/trainer/SegmentationTrainer.java
@@ -1,5 +1,6 @@
 package org.grobid.trainer;
 
+import net.sf.saxon.lib.SaxonOutputKeys;
 import org.grobid.core.GrobidModels;
 import org.grobid.core.GrobidModels.Flavor;
 import org.grobid.core.exceptions.GrobidException;
@@ -76,25 +77,22 @@ public class SegmentationTrainer extends AbstractTrainer {
                                        double splitRatio) {
         int totalExamples = 0;
         try {
-            System.out.println("sourceTEIPathLabel: " + sourceTEIPathLabel);
-            System.out.println("sourceRawPathLabel: " + sourceRawPathLabel);
-            System.out.println("trainingOutputPath: " + trainingOutputPath);
-            System.out.println("evalOutputPath: " + evalOutputPath);
+            LOGGER.info("sourceTEIPathLabel: " + sourceTEIPathLabel);
+            LOGGER.info("sourceRawPathLabel: " + sourceRawPathLabel);
+            LOGGER.info("trainingOutputPath: " + trainingOutputPath);
+            LOGGER.info("evalOutputPath: " + evalOutputPath);
 
             // we need first to generate the labeled files from the TEI annotated files
             File input = new File(sourceTEIPathLabel);
             // we process all tei files in the output directory
-            File[] refFiles = input.listFiles(new FilenameFilter() {
-                public boolean accept(File dir, String name) {
-                    return name.endsWith(".tei.xml") || name.endsWith(".tei");
-                }
-            });
+            File[] refFiles = input.listFiles((dir, name) ->
+                    name.endsWith(".tei.xml") || name.endsWith(".tei"));
 
             if (refFiles == null) {
                 return 0;
             }
 
-            System.out.println(refFiles.length + " tei files");
+            LOGGER.info(refFiles.length + " tei files");
 
             // the file for writing the training data
             OutputStream os2 = null;
@@ -148,83 +146,71 @@ public class SegmentationTrainer extends AbstractTrainer {
                         continue;
                     }
 
-                    // removing the @newline
-                    /*List<String> newLabeled = new ArrayList<String>();
-                    for(String label : labeled) {
-                        if (!label.startsWith("@newline"))
-                            newLabeled.add(label);
-                    } 
-                    labeled = newLabeled;*/
-
-/*StringBuilder temp = new StringBuilder();
-for(String label : labeled) {
-    temp.append(label);
-}
-FileUtils.writeStringToFile(new File("/tmp/expected-"+name+".txt"), temp.toString());*/
-                
                     int q = 0;
-                    BufferedReader bis = new BufferedReader(
-                        new InputStreamReader(new FileInputStream(theRawFile), StandardCharsets.UTF_8));
                     StringBuilder segmentation = new StringBuilder();
-                    String line = null;
-                    int l = 0;
                     String previousTag = null;
                     int nbInvalid = 0;
-                    while ((line = bis.readLine()) != null) {
-                        l++;
-                        int ii = line.indexOf(' ');
-                        String token = null;
-                        if (ii != -1) {
-                            token = line.substring(0, ii);
-                            // unicode normalisation of the token - it should not be necessary if the training data
-                            // has been gnerated by a recent version of grobid
-                            token = UnicodeUtil.normaliseTextAndRemoveSpaces(token);
-                        }
-                        // we get the label in the labelled data file for the same token
-                        for (int pp = q; pp < labeled.size(); pp++) {
-                            String localLine = labeled.get(pp);
-                            StringTokenizer st = new StringTokenizer(localLine, " \t");
-                            if (st.hasMoreTokens()) {
-                                String localToken = st.nextToken();
+                    int lineNumber = 0;
+                    try (BufferedReader bis = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(theRawFile), StandardCharsets.UTF_8))) {
+                        String line;
+                        while ((line = bis.readLine()) != null) {
+                            lineNumber++;
+                            int ii = line.indexOf(' ');
+                            String token = null;
+                            if (ii != -1) {
+                                token = line.substring(0, ii);
                                 // unicode normalisation of the token - it should not be necessary if the training data
-                                // has been gnerated by a recent version of grobid
-                                localToken = UnicodeUtil.normaliseTextAndRemoveSpaces(localToken);
-                                if (localToken.equals(token)) {
-                                    String tag = st.nextToken();
-                                    segmentation.append(line).append(" ").append(tag);
-                                    previousTag = tag;
-                                    q = pp + 1;
-                                    nbInvalid = 0;
-                                    //pp = q + 10;
+                                // has been generated by a recent version of grobid
+                                token = UnicodeUtil.normaliseTextAndRemoveSpaces(token);
+                            }
+                            // we get the label in the labelled data file for the same token
+                            for (int pp = q; pp < labeled.size(); pp++) {
+                                String localLine = labeled.get(pp);
+                                StringTokenizer st = new StringTokenizer(localLine, " \t");
+                                if (st.hasMoreTokens()) {
+                                    String localToken = st.nextToken();
+                                    localToken = UnicodeUtil.normaliseTextAndRemoveSpaces(localToken);
+                                    if (localToken.equals(token)) {
+                                        String tag = st.nextToken();
+                                        segmentation.append(line).append(" ").append(tag);
+                                        previousTag = tag;
+                                        q = pp + 1;
+                                        nbInvalid = 0;
+                                        break;
+                                    }
+                                }
+                                if (pp - q > 5) {
+                                    nbInvalid++;
+                                    LOGGER.debug("{} / TEI and raw file unsynchronized at raw line {}, token '{}' not found in TEI", name, lineNumber, token);
+                                    // let's reuse the latest tag
+                                    if (previousTag != null)
+                                        segmentation.append(line).append(" ").append(previousTag);
                                     break;
                                 }
                             }
-                            if (pp - q > 5) {
-                                //LOGGER.warn(name + " / Segmentation trainer: TEI and raw file unsynchronized at raw line " + l + " : " + localLine);
-                                nbInvalid++;
-                                // let's reuse the latest tag
-                                if (previousTag != null)
-                                   segmentation.append(line).append(" ").append(previousTag);
+                            if (nbInvalid > 20) {
+                                // too many consecutive synchronization issues
                                 break;
                             }
                         }
-                        if (nbInvalid > 20) {
-                            // too many consecutive synchronization issues
-                            break;
-                        }
                     }
-                    bis.close();
 
                     if (nbInvalid < 10) {
-                        if ((writer2 == null) && (writer3 != null))
-                            writer3.write(segmentation.toString() + "\n");
-                        if ((writer2 != null) && (writer3 == null))
-                            writer2.write(segmentation.toString() + "\n");
-                        else {
+                        String output = segmentation + "\n";
+                        if ((writer2 == null) && (writer3 != null)) {
+                            writer3.write(output);
+                        } else if ((writer2 != null) && (writer3 == null)) {
+                            writer2.write(output);
+                        } else if (writer2 != null && writer3 != null) {
                             if (Math.random() <= splitRatio)
-                                writer2.write(segmentation.toString() + "\n");
+                                writer2.write(output);
                             else
-                                writer3.write(segmentation.toString() + "\n");
+                                writer3.write(output);
+                        }
+                        totalExamples++;
+                        if (nbInvalid > 0) {
+                            LOGGER.warn(name + " / found "+nbInvalid+" synchronization mismatches, however the file is anyway accepted!");
                         }
                     } else {
                         LOGGER.error("{} / too many synchronization issues, file not used in training data and to be fixed!", name);
